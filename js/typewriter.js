57ec8969b77c68294ce68bfb51591672793fbec6
Hivie <rayenattia@gmail.com>
2025-02-06 22:15:48 +0100

    move typewriter text to script

--- js/typewriter.js
+++ js/typewriter.js
@@ -1,6 +1,6 @@
 function setupTypewriter() {
     const subtitle = document.querySelector(".subtitle");
-    const text = subtitle.textContent;
+    const text = "osu!team, software engineering";
 
     subtitle.textContent = "";
     subtitle.style.animation = "blink-caret 0.5s normal infinite";
